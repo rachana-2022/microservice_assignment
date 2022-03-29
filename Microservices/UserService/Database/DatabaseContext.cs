ca6d983d301931a1eebf829b0de8c9616d9d3c80
Kulkarni <rachana.kulkarni@dxc.com>
2022-03-29 17:16:16 +0530

    File Added

---
+++ Microservices/UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source = IN-DMGHZH3; database = UserMicroservice; Trusted_Connection = true;");
+        }
+    }
+}
